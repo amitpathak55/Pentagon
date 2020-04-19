4a38c3c0a7f0522f28a09e06d81373f9362324a7
Amit Pathak <amitpathak@my.unt.edu>
2020-04-18 22:08:38 -0500

    merge update

--- sprint2/core/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminAuthConfig(AppConfig):
-    name = 'admin_auth'
--- sprint2/student/templatetags/add_class.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='add_class')
-def add_class(elem, arg):
-    elem.field.widget.attrs.update({'class' : arg})
-    return elem
